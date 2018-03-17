92f836aa66fef9f39493173bd4c4abe02f3ddc7a
Aditya Pednekar <aditya.ped@oschannel.com>
2018-03-17 10:26:57 +0000

    cmc script v2 now ready

--- cmc_live_prices_v2.py
+++ cmc_live_prices_v2.py
@@ -12,37 +12,44 @@ def SendData():
     sitename = "cmc"
     for i in AllData:
       if i['id'] in cryptos:
-        new_key_1 = sitename + '-' + i['id'] + '-' + '-last'
-        new_value_1 = i['id']['price_inr']
+        new_key_1 = sitename + '-' + i['symbol'].lower() + '-last'
+        new_val_1 = i['price_inr']
         new_dict.update({new_key_1:new_val_1})
-        new_key_2 = sitename + '-' + i['id'] + '-' + '-usd'
-        new_value_2 = i['id']['price_inr']
+        new_key_2 = sitename + '-' + i['symbol'].lower() + '-usd'
+        new_val_2 = i['price_inr']
         new_dict.update({new_key_2:new_val_2})
-        new_key_3 = sitename + '-' + i['id'] + '-' + '-24h-pct-change'
-        new_value_3 = i['id']['percent_change_24h']
+        new_key_3 = sitename + '-' + i['symbol'].lower() + '-24h-pct-change'
+        new_val_3 = i['percent_change_24h']
         new_dict.update({new_key_3:new_val_3})
-        new_key_4 = sitename + '-' + i['id'] + '-' + '24h-volume-usd'
-        new_value_4 = i['id']['24h_volume_usd']
+        new_key_4 = sitename + '-' + i['symbol'].lower() + '-24h-volume-usd'
+        new_val_4 = i['24h_volume_usd']
         new_dict.update({new_key_4:new_val_4})
-        new_key_5 = sitename + '-' + i['id'] + '-' + '24h-volume-inr'
-        new_value_5 = i['id']['24h_volume_inr']
+        new_key_5 = sitename + '-' + i['symbol'].lower() + '-24h-volume-inr'
+        new_val_5 = i['24h_volume_inr']
         new_dict.update({new_key_5:new_val_5})
+        new_key_6 = sitename + '-' + i['symbol'].lower() + '-rank'
+        new_val_6 = i['rank']
+        new_dict.update({new_key_6:new_val_6})
     g.send_dict(new_dict)
     r.hmset("Cmc", new_dict)
   else:
     pass
 
 if __name__=="__main__":
+
   cryptos = ["bitcoin", "ethereum", "ripple", "bitcoin-cash", "litecoin", "cardano", "neo", "stellar", "eos", "monero", "dash", "iota", "nem", "tron", "ethereum-classic", "vechain", "lisk", "nano", "omisego", "qtum", "bitcoin-gold", "zcash", "verge", "dogecoin", "siacoin", "ontology", "aeternity", "0x", "electroneum", "digibyte", "golem-network-tokens", "gas", "basic-attention-token", "zcoin", "deepbrain-chain", "red-pulse", "request-network", "zilla" ]
+
   URL = "https://api.coinmarketcap.com/v1/ticker/?convert=INR&limit=1000"
   SERVER = '127.0.0.1' 
   CARBON_PORT = 2003
   REDIS_PORT = 6379
   r = redis.Redis(host=SERVER, port=REDIS_PORT, db=0)
   g = graphitesend.init(graphite_server=SERVER, graphite_port=CARBON_PORT, group='metrics-cmc')
+
+
   while True:
-    SendData()
     time.sleep(6)
+    SendData()
 
 
 
