cddb582e4bef4fd9b6eeffef1a6c4891aae787e6
Aditya Pednekar <aditya.ped@oschannel.com>
2018-03-17 13:05:50 +0000

    building a docker compose template for graphite and graphana

---
+++ koinex_live_prices_v2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import requests, graphitesend, redis
+import json, time
+
+def SendData():
+  response = requests.get(URL)
+
+  if response.status_code == 200:
+    AllData = response.json()
+    new_dict = {}
+    sitename = "koinex"
+    for crypto in AllData['stats']:
+      crypto_name = crypto
+      new_key_1 = sitename + '-' + crypto_name.lower() + '-last'
+      new_val_1 = AllData['stats'][crypto_name]['last_traded_price']
+      new_dict.update({new_key_1:new_val_1})
+      new_key_2 = sitename + '-' + crypto_name.lower() + '-buy'
+      new_val_2 = AllData['stats'][crypto_name]['highest_bid']
+      new_dict.update({new_key_2:new_val_2})
+      new_key_3 = sitename + '-' + crypto_name.lower() + '-sell'
+      new_val_3 = AllData['stats'][crypto_name]['lowest_ask']
+      new_dict.update({new_key_3:new_val_3})
+      new_key_4 = sitename + '-' + crypto_name.lower() + '-24h-volume-inr'
+      new_val_4 = AllData['stats'][crypto_name]['vol_24hrs']
+      new_dict.update({new_key_4:new_val_4})
+    g.send_dict(new_dict)
+    r.hmset("Koinex", new_dict) 
+     
+  else:
+    pass
+
+if __name__=="__main__":
+    
+    URL = "https://koinex.in/api/ticker"
+    SERVER = '127.0.0.1' 
+    CARBON_PORT = 2003
+    REDIS_PORT = 6379
+    r = redis.Redis(host=SERVER, port=REDIS_PORT, db=0)
+    g = graphitesend.init(graphite_server=SERVER, graphite_port=CARBON_PORT, group='metrics-koinex')
+  
+    while True:
+        SendData()
+        time.sleep(22)
